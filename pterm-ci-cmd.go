078d0cad07bb1fcde9658f61c57df0a7b058bd51
MarvinJWendt <git@marvinjwendt.com>
2021-05-20 17:38:37 +0200

    refactor: update macOS install command

--- pterm-ci-cmd.go
+++ pterm-ci-cmd.go
@@ -70,8 +70,8 @@ It should not be used outside the development of this tool.`,
 		project.Long = rootCmd.Long
 
 		project.InstallCommandWindows = pterm.Sprintf(`iwr -useb instl.sh/%s/windows | iex`, project.ProjectPath)
-		project.InstallCommandLinux = pterm.Sprintf(`curl -fsSL instl.sh/%s/linux | sudo bash`, project.ProjectPath)
-		project.InstallCommandMacOS = pterm.Sprintf(`/bin/bash -c "$(curl -fsSL instl.sh/%s/macos)"`, project.ProjectPath)
+		project.InstallCommandLinux = pterm.Sprintf(`curl -fsSL instl.sh/%s/linux | bash`, project.ProjectPath)
+		project.InstallCommandMacOS = pterm.Sprintf(`curl -fsSL instl.sh/%s/macos | bash "`, project.ProjectPath)
 
 		project.GitHubPagesURL = pterm.Sprintf("https://%s.github.io/%s", project.UserName, project.RepoName)
 
